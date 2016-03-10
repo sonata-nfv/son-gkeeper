eb454a7b6c4cad1d6d3514a8e6141c532c5d3aa4
José Bonnet <jose.bonnet@gmail.com>
2016-03-10 15:07:38 +0000

    Add API root with tests

--- son-gtkapi/spec/spec_helper.rb
+++ son-gtkapi/spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # spec/spec_helper.rb
 require 'rack/test'
 require 'rspec'
+require 'webmock/rspec'
 
 ENV['RACK_ENV'] = 'test'
 
@@ -8,7 +9,7 @@ require File.expand_path '../../app.rb', __FILE__
 
 module RSpecMixin
   include Rack::Test::Methods
-  def app() App end
+  def app() Sinatra::Application end
 end
 
 RSpec.configure { |c| c.include RSpecMixin }
