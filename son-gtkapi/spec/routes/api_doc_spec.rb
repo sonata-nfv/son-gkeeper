9a510d0b6dfe85ba342fb80e1c9ead10a8b68683
José Bonnet <jose.bonnet@gmail.com>
2016-03-22 15:43:04 +0000

    Resolves [sonata-nfv/son-gkeeper#30] Add /packages POST

--- son-gtkapi/spec/routes/api_doc_spec.rb
+++ son-gtkapi/spec/routes/api_doc_spec.rb
@@ -18,3 +18,20 @@ RSpec.describe 'Test /api-doc Path access' do
   end
 end
 
+# it 'saves string to the file system' do
+#  string_changer = StringChanger.new
+#  File.stub(:write)
+#
+#  string_changer.reverse_and_save('example string')
+#
+#  expect(File).
+#    to have_received(:write).
+#    with('example_file', 'gnirts elpmaxe').
+#    once
+#end
+
+
+#    it "redirects to /play" do
+#      follow_redirect!
+#      last_request.path.should == '/play'
+#    end
