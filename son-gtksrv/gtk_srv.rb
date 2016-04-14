4aacf86ef1607752468e532f685bf092cb07ac34
José Bonnet <jose.bonnet@gmail.com>
2016-04-14 13:38:31 +0100

    Draft version of Service Management module.
    This version has only GET /services, with the fake Catalogue

---
+++ son-gtksrv/gtk_srv.rb
@@ -0,0 +1,51 @@
+## SONATA - Gatekeeper
+##
+## Copyright 2015-2017 Portugal Telecom Inovação/Altice Labs
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##   http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+require 'sinatra/base'
+require 'sinatra/config_file'
+require 'sinatra/cross_origin'
+require 'sinatra/reloader'
+
+# Require the bundler gem and then call Bundler.require to load in all gems listed in Gemfile.
+require 'bundler'
+Bundler.require :default, ENV['RACK_ENV'].to_sym
+
+require_relative 'routes/init'
+require_relative 'helpers/init'
+require_relative 'models/init'
+
+# https://github.com/achiu/rack-parser
+#use Rack::Parser, :content_types => { 'application/json' => Proc.new { |body| ::MultiJson.decode body } }
+
+# Main class supporting the Gatekeeper's Service Management micro-service
+class GtkSrv < Sinatra::Base
+  register Sinatra::ConfigFile
+  register Sinatra::CrossOrigin
+  register Sinatra::Reloader
+  
+  helpers GtkSrvHelper
+  
+  set :root, File.dirname(__FILE__)
+  set :public_folder, File.join(File.dirname(__FILE__), 'public')
+  set :bind, '0.0.0.0'
+  set :time_at_startup, Time.now.utc
+  set :environments, %w(development test integration qualification demonstration)
+  set :environment, ENV['RACK_ENV'] || :development
+  config_file File.join(root, 'config', 'services.yml')
+
+	use Rack::Session::Cookie, key: 'rack.session', domain: 'foo.com', path: '/', expire_after: 2592000, secret: '$0nata'
+	enable :logging
+  enable :cross_origin
+end
