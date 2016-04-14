26537fb2138457dcd36eda4856b6c737106fee5f
José Bonnet <jose.bonnet@gmail.com>
2016-04-14 16:54:15 +0100

    Fix non-utf8 characters

--- son-gtksrv/gtk_srv.rb
+++ son-gtksrv/gtk_srv.rb
@@ -1,6 +1,6 @@
 ## SONATA - Gatekeeper
 ##
-## Copyright 2015-2017 Portugal Telecom Inovação/Altice Labs
+## Copyright 2015-2017 Portugal Telecom Inovacao/Altice Labs
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 ## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
+# encoding: utf-8
 require 'sinatra/base'
 require 'sinatra/config_file'
 require 'sinatra/cross_origin'
