50e467269a08ffc807b02f7f344e4b7793aa822d
José Bonnet <jose.bonnet@gmail.com>
2016-04-25 22:34:34 +0100

    Fix missing helper function keyed_hash()

--- son-gtksrv/helpers/service.rb
+++ son-gtksrv/helpers/service.rb
@@ -31,4 +31,8 @@ module GtkSrvHelper
     uuid.match /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/
     uuid == $&
   end
+  
+  def keyed_hash(hash)
+    Hash[hash.map{|(k,v)| [k.to_sym,v]}]
+  end
 end
