5ed603500d6b79f9fe89b93fd61387674f60beaa
fporto-optare <fporto@optaresolutions.com>
2016-05-13 09:43:30 +0200

    GTK SRV request POST implementation

--- son-gtksrv/routes/request.rb
+++ son-gtksrv/routes/request.rb
@@ -17,9 +17,57 @@
 require 'json' 
 require 'pp'
 require 'addressable/uri'
+require 'yaml'
+require 'bunny'
+require "thread"
 
 class GtkSrv < Sinatra::Base
+  attr_reader
+  attr_accessor :correlation_ids
+  
+  def initialize()
+	
+    conn = Bunny.new(GtkSrv.mqserver['url'],:automatically_recover => false)
+    conn.start
+    self.correlation_ids=Hash.new
     
+    ch   = conn.create_channel
+    @ch             = ch
+    @x              = ch.topic("son-kernel", :auto_delete => false)
+    @server_queue   = "service.instances.create"
+    
+    that = self
+    @q = ch.queue(@server_queue, :auto_delete => true).bind(@x, :routing_key => @server_queue)
+    
+    @q.subscribe do |delivery_info, properties, payload|
+     begin	
+      if properties[:headers]!=nil &&
+         properties[:headers]['type']=='reply' && 
+	 self.correlation_ids[properties[:correlation_id]] != nil 
+        
+	
+	logger.debug(payload.class)
+	#logger.debug("Received #{payload}")
+	
+	parsed_payload = YAML.load(payload)
+	
+	request = Request.find_by(service_uuid: properties[:correlation_id])
+	
+	request['status']=parsed_payload['status']
+	
+	request.save
+	
+	self.correlation_ids.delete(properties[:correlation_id])
+       end
+       rescue Exception => e
+         logger.debug(e.message)
+	 logger.debug(e.backtrace.inspect)
+       end
+      end
+        ObjectSpace.define_finalizer( self, proc { conn.close } )
+  end
+
+  
   # GETs a request, given an uuid
   get '/requests/:uuid/?' do
     logger.debug "GtkSrv: entered GET /requests/#{params[:uuid]}"
@@ -27,6 +75,13 @@ class GtkSrv < Sinatra::Base
     json_request = json(request, { root: false })
     halt 206, json_request if request
     json_error 404, "GtkSrv: Request #{params[:uuid]} not found"    
+  end
+  
+  get '/mytest/?' do 
+    logger.debug "PRueba!!!" 
+    logger.debug "GtkSrv: entered GET /requests/#{params[:uuid]}" 
+    halt 200, "ok"
+    
   end
 
   # GET many requests
@@ -57,15 +112,47 @@ class GtkSrv < Sinatra::Base
     logger.info "GtkSrv: entered POST /requests with params=#{params}"
     
     begin
+      logger.debug "GtkSrv: entered POST /requests with service uuid=#{params[:service_uuid]}"
+      
       request = Request.create(:service_uuid => params[:service_uuid])
+
       json_request = json(request, { root: false })
-      pp json_request
+      
+      service = JSON.parse(RequestManagerService.find_services_by_uuid(params[:service_uuid]))
+
+      start_request=Hash.new
+      
+      start_request['NSD']=service
+
+      counter=1
+      
+        service['network_functions'].each do |nfd| 
+      
+         complete_nfd=JSON.parse(RequestManagerService.find_function(nfd['vnf_name'],nfd['vnf_vendor'],nfd['vnf_version']))
+         complete_nfd=complete_nfd[0]
+      
+         start_request["VNFD#{counter}"]=complete_nfd
+      
+         counter= counter + 1
+      
+        end
+            
+      start_request_yml = YAML.dump(start_request)
+      
+      logger.debug(start_request_yml)
+
+      smresponse=call_sm(start_request_yml,params[:service_uuid])
+      
       logger.info 'GtkSrv: returning POST /requests with request='+json_request
+      
       halt 201, json_request
-    rescue e
-      logger.info "GtkSrv: returning POST /requests with 'GtkSrv: Not possible to save the request'"
-      json_error 400, 'GtkSrv: Not possible to save the request'
-    end
+      
+      rescue Exception => e
+         logger.debug(e.message)
+	 logger.debug(e.backtrace.inspect)
+	 halt 500, 'Internal server error'
+	 
+      end
   end
 
   # PUTs an update on an existing instantiation request, given its UUID
@@ -81,5 +168,19 @@ class GtkSrv < Sinatra::Base
       json_error 400, 'GtkSrv: Not possible to update the request'
     end 
   end
+  
+  
+  def call_sm(n,correlation_id)
+   
+    self.correlation_ids[correlation_id]=correlation_id
+    logger.debug(correlation_id)
+    @x.publish(n.to_s,
+      :routing_key    => @server_queue,
+      :correlation_id => correlation_id,
+      :reply_to       => @q.name)
+
+  end
+  
+  
 end
 
