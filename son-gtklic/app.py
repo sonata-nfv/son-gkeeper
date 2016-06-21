ae5ff3963835a35e2ff8509595177efc6d58fe9c
Bruno Silva <brunomiguelsilva@ua.pt>
2016-06-21 17:08:09 +0100

    added urls for licenses and services

--- son-gtklic/app.py
+++ son-gtklic/app.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from views.types import TypesList, Types
+from views.licenses import Licenses, LicensesList
+form vies.services import Services
 
 app = Flask(__name__)
 
@@ -10,6 +12,10 @@ api = Api(app)
 api.add_resource(TypesList, '/types')
 api.add_resource(Types, '/types/<typeID>')
 
+api.add_resource(Services, '/services')
+
+api.add_resource(Licenses, '/licenses')
+api.add_resource(LicensesList, '/licenses/<licenseID>')
 
 if __name__ == '__main__':
     app.run(port=5000, host="0.0.0.0")
