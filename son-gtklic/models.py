2e62194795eea12909e1110f60a82b142ef0c98b
Bruno Silva <brunomiguelsilva@ua.pt>
2016-06-27 13:49:27 +0100

    fixed license type model to support license duration

--- son-gtklic/models.py
+++ son-gtklic/models.py
@@ -10,15 +10,17 @@ class Type(Base):
 
     type_uuid = Column(String, primary_key=True, default=str(uuid.uuid4()))
     type = Column(String)
+    duration = Column(Integer)
     active = Column(Boolean, default=True)
 
-    def __init__(self, arg_type):
+    def __init__(self, arg_type, duration):
         self.type_uuid = str(uuid.uuid4())
         self.type = arg_type
+        self.duration = duration
         self.active = True
 
     def __repr__(self):
-        return "<License(uuid='%s', type='%s', active='%s')>" % (self.type_uuid, self.type, self.active)
+        return "<License(uuid='%s', type='%s', duration='%s', active='%s')>" % (self.type_uuid, self.type, self.duration, self.active)
 
     @property
     def serialize(self):
@@ -26,6 +28,7 @@ class Type(Base):
         return {
             'type_uuid': self.type_uuid,
             'type': self.type,
+            'duration': self.duration,
             'active': self.active
         }
 
