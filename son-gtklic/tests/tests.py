d296a722c09ec6d5077186cdf1732c69eb68f301
Bruno Silva <brunomiguelsilva@ua.pt>
2016-07-03 16:51:37 +0100

    testing services done!     -testing adding service     -testing getting services     -testing deleting (deactivating) a service

--- son-gtklic/tests/tests.py
+++ son-gtklic/tests/tests.py
@@ -4,39 +4,37 @@ import unittest
 import logging
 import json
 
-
-from flask_testing import TestCase, LiveServerTestCase
-from flask_testing.utils import ContextVariableDoesNotExist
-
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from settings import BASE_DIR
 from app import app
 import db
+
+
 type_uuid = None
+service_uuid = None
+
+
 class TestCase(unittest.TestCase):
 
     def setUp(self):
-        #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'test.db')
         app.config['TESTING'] = True
         self.app = app.test_client()
-        
         db.init_db()
 
     def tearDown(self):
         pass
 
     def test_add_type(self):
-        global type_uuid 
+        global type_uuid
 
         response = self.app.post("/types", data=dict(type="Test", duration=30))
         self.assertEqual(response.status_code, 200)
         resp_json = json.loads(response.data)
-        
+
         log = logging.getLogger()
         log.debug(response.status_code)
         log.debug(response.data)
-        
+
         type_uuid = resp_json["type_uuid"]
         log.debug(resp_json["type_uuid"])
         duration = resp_json["duration"]
@@ -45,13 +43,12 @@ class TestCase(unittest.TestCase):
         self.assertEqual(duration, 30)
         self.assertEqual(desc, "Test")
 
-
     def test_get_type(self):
 
         response = self.app.get("/types")
         self.assertEqual(response.status_code, 200)
         resp_json = json.loads(response.data)
-        
+
         log = logging.getLogger()
         log.debug(response.status_code)
 
@@ -66,9 +63,46 @@ class TestCase(unittest.TestCase):
         response = self.app.delete("/types/" + type_uuid)
         self.assertEqual(response.status_code, 200)
         resp_json = json.loads(response.data)
-        
+
         log = logging.getLogger()
         log.debug(response.status_code)
+
+        self.assertFalse(resp_json["active"])
+
+    def test_add_service(self):
+        global service_uuid
+
+        response = self.app.post("/services", data=dict(description="Test",
+                                                        expiringDate="22-07-20 13:46",
+                                                        startingDate="03-07-20 13:46",
+                                                        active=True))
+        self.assertEqual(response.status_code, 200)
+        resp_json = json.loads(response.data)
+
+        service_uuid = resp_json["service_uuid"]
+        expiringDate = resp_json["expiringDate"]
+        desc = resp_json["description"]
+
+        self.assertEqual(expiringDate, "22-07-20 13:46")
+        self.assertEqual(desc, "Test")
+
+    def test_get_service(self):
+
+        response = self.app.get("/services")
+        self.assertEqual(response.status_code, 200)
+        resp_json = json.loads(response.data)
+
+        service_list = []
+        for i in resp_json["services"]:
+            service_list.append(i["service_uuid"])
+
+        self.assertTrue(service_uuid in service_list)
+
+    def test_delete_service(self):
+
+        response = self.app.delete("/services", data=dict(service_uuid=service_uuid))
+        self.assertEqual(response.status_code, 200)
+        resp_json = json.loads(response.data)
 
         self.assertFalse(resp_json["active"])
 
