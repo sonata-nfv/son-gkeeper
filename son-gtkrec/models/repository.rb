e63e6e67c3ff0a64731e704f67a42925533e31c1
José Bonnet <jose.bonnet@gmail.com>
2016-07-05 11:30:56 +0100

    Add logging messages to find out why a model isn't being required

--- son-gtkrec/models/repository.rb
+++ son-gtkrec/models/repository.rb
@@ -14,9 +14,7 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 # encoding: utf-8
-require 'tempfile'
 require 'json'
-require 'pp'
 
 class Repository
   
