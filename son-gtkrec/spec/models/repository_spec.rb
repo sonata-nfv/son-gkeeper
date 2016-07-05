ca7c9048c7e2f635340a85f4374eb5cf5abad6e7
José Bonnet <jose.bonnet@gmail.com>
2016-07-05 12:23:05 +0100

    Fix REPOSITORIES_URL config, add /spec folder

---
+++ son-gtkrec/spec/models/repository_spec.rb
@@ -0,0 +1,30 @@
+## SONATA - Gatekeeper
+##
+## Copyright 2015-2017 Portugal Telecom Inovacao/Altice Labs
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##   http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+require_relative '../spec_helper'
+
+RSpec.describe Repository, type: :model do
+  describe '#find_by_uuid' do
+  end
+  
+  describe '#find' do
+  end
+  
+  describe '.to_file' do
+  end
+  
+  describe '.from_file' do
+  end
+end
