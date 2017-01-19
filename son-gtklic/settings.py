f1514ccf4b48e1a06d2c5c894a6a9b09573bda45
José Bonnet <jose.bonnet@gmail.com>
2017-01-19 16:20:49 +0000

    Fix (I was expecting) the running port of son-gtklic

--- son-gtklic/settings.py
+++ son-gtklic/settings.py
@@ -5,7 +5,7 @@ POSTGRES_USER = os.environ.get('POSTGRES_USER') or 'sonata'
 POSTGRES_DB = os.environ.get('POSTGRES_DB') or 'licensemanager'
 DATABASE_HOST = os.environ.get('DATABASE_HOST') or 'postgres'
 DATABASE_PORT = os.environ.get('DATABASE_PORT') or '5432'
-PORT = os.environ.get('DATABASE_HOST') or '5000'
+PORT = os.environ.get('PORT') or '6000'
 
 SQLALCHEMY_DATABASE_URI = ('postgresql://%s:%s@%s:%s/%s' %(POSTGRES_USER, POSTGRES_PASSWORD, DATABASE_HOST, DATABASE_PORT, POSTGRES_DB))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
