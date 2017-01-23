dd71a7cbbf6e25ac21de9c6e165d2bfa692a4d13
srodriguezOPT <srodriguez@optaresolutions.com>
2017-01-23 13:19:36 +0100

    Y2 new features: son-gtkkpi module

--- son-gtkkpi/gtk_kpi.rb
+++ son-gtkkpi/gtk_kpi.rb
@@ -26,47 +26,47 @@
 ## acknowledge the contributions of their colleagues of the SONATA 
 ## partner consortium (www.sonata-nfv.eu).
 # encoding: utf-8
-# Set environment
 ENV['RACK_ENV'] ||= 'production'
 
 require 'sinatra/base'
+require 'sinatra/json'
 require 'sinatra/config_file'
 require 'sinatra/cross_origin'
 require 'sinatra/reloader'
-require 'zip'
+require 'sinatra/activerecord'
 require 'sinatra/logger'
-require 'sinatra/namespace'
 
 # Require the bundler gem and then call Bundler.require to load in all gems listed in Gemfile.
 require 'bundler'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
 
-['helpers', 'routes', 'models'].each do |dir|
+['helpers', 'routes', 'models', 'lib'].each do |dir|
   Dir[File.join(File.dirname(__FILE__), dir, '**', '*.rb')].each do |file|
     require file
   end
 end
 
-# Concentrates all the REST API of the Gatekeeper
-class GtkApi < Sinatra::Base
+# Main class supporting the Gatekeeper's Service Management micro-service
+class GtkKpi < Sinatra::Base
   register Sinatra::ConfigFile
   register Sinatra::CrossOrigin
   register Sinatra::Reloader
+  register Sinatra::ActiveRecordExtension
   register Sinatra::Logger
-  register Sinatra::Namespace
+  set :logger_level, :debug # or :fatal, :error, :warn, :info
   
-  helpers GtkApiHelper
-
+  helpers GtkKpiHelper
+  
   set :root, File.dirname(__FILE__)
   set :public_folder, File.join(File.dirname(__FILE__), 'public')
   set :bind, '0.0.0.0'
-  set :files, File.join(settings.public_folder, 'files')
   set :time_at_startup, Time.now.utc
   set :environments, %w(development test integration qualification demonstration)
   set :environment, ENV['RACK_ENV'] || :development
-  config_file File.join(root, 'config', 'services.yml.erb')
+  config_file File.join( [root, 'config', 'prometheus.yml.erb'] )
   
-  use Rack::Session::Cookie, key: 'rack.session', domain: 'foo.com', path: '/', expire_after: 2592000, secret: '$0nata'
+    
+  use Rack::Session::Cookie, :key => 'rack.session', :domain => 'foo.com', :path => '/', :expire_after => 2592000, :secret => '$0nata'
   
   # Logging
 	enable :logging
@@ -75,23 +75,8 @@ class GtkApi < Sinatra::Base
   logfile = File.open(File.join('log', ENV['RACK_ENV'])+'.log', 'a+')
   logfile.sync = true
   logger = Logger.new(logfile)
-  
+    
   enable :cross_origin
 
-  # TODO: make this relationship loosely coupled
-  PackageManagerService.config(url: settings.pkgmgmt, logger: logger)
-  ServiceManagerService.config(url: settings.srvmgmt, logger: logger)
-  FunctionManagerService.config(url: settings.fnctmgmt, logger: logger)
-  RecordManagerService.config(url: settings.recmgmt, logger: logger)
-  set :licence_management, LicenceManagerService.new(settings.licmgmt, logger)
-  set :vim_management, VimManagerService.new(settings.vimmgmt, logger)
-  set :kpi_management, KpiManagerService.new(settings.kpimgmt, logger)
-  
-  Zip.setup do |c|
-    c.unicode_names = true
-    c.on_exists_proc = true
-    c.continue_on_exists_proc = true
-  end
-  MODULE = 'GtkApi'
-  logger.info(MODULE) {"Started at #{settings.time_at_startup}"}
+  logger.info "GtkKpi started at #{settings.time_at_startup}"
 end
---
+++ son-gtkkpi/lib/prometheus/client/version.rb
@@ -0,0 +1,7 @@
+# encoding: UTF-8
+
+module Prometheus
+  module Client
+    VERSION = '0.7.0.beta1'
+  end
+end
