5fbdce9cdc15c67d1383ba9ee80398dc42403241
José Bonnet <jose.bonnet@gmail.com>
2017-04-10 14:45:19 +0100

    Fix too drastic simplification of Record model (caused recurssiveness)

--- son-gtkapi/routes/record.rb
+++ son-gtkapi/routes/record.rb
@@ -50,7 +50,7 @@ class GtkApi < Sinatra::Base
       @offset ||= params[:offset] ||= DEFAULT_OFFSET 
       @limit ||= params[:limit] ||= DEFAULT_LIMIT
   
-      records = RecordManagerService.find(params)
+      records = RecordManagerService.find_records(params)
       case records[:status]
       when 200
         logger.debug(log_message) {"leaving with #{records}"}
@@ -69,7 +69,7 @@ class GtkApi < Sinatra::Base
       unless params[:uuid].nil?
         logger.debug(method) {'entered'}
         json_error 400, 'Invalid Instance UUID' unless valid? params[:uuid]
-        record = RecordManagerService.find_by_uuid(params[:uuid])
+        record = RecordManagerService.find_records_by_uuid(params[:uuid])
         case record[:status]
         when 200
           logger.debug(log_message) {"leaving with #{record}"}
@@ -100,7 +100,7 @@ class GtkApi < Sinatra::Base
         end
       
         # here we have the 
-        descriptor = RecordManagerService.find_service_by_uuid(body_params['latest_nsd_id'])
+        descriptor = RecordManagerService.find_records_by_uuid(kind: 'services', uuid: body_params['latest_nsd_id'])
         if descriptor
           logger.debug(method) {"found #{descriptor}"}
 
