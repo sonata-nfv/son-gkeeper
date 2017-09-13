91435e39f9c08d28637dce8a28800f916d48b273
José Bonnet <jose.bonnet@gmail.com>
2017-09-13 09:41:46 +0100

    Fix default ports (overriden in most places, but...)

--- son-gtklic/settings.py
+++ son-gtklic/settings.py
@@ -5,7 +5,7 @@ POSTGRES_USER = os.environ.get('POSTGRES_USER') or 'sonata'
 POSTGRES_DB = os.environ.get('POSTGRES_DB') or 'licensemanager'
 DATABASE_HOST = os.environ.get('DATABASE_HOST') or 'localhost'
 DATABASE_PORT = os.environ.get('DATABASE_PORT') or '5432'
-PORT = os.environ.get('PORT') or '5000'
+PORT = os.environ.get('PORT') or '5900'
 
 SQLALCHEMY_DATABASE_URI = ('postgresql://%s:%s@%s:%s/%s' %(POSTGRES_USER, POSTGRES_PASSWORD, DATABASE_HOST, DATABASE_PORT, POSTGRES_DB))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
