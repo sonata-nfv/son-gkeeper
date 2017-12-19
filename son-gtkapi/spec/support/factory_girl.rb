9118b585ea5b0298a09f6c008fd8cb695704080f
José Bonnet <jose.bonnet@gmail.com>
2017-12-19 16:00:33 +0000

    Remove dependency from factory_girl

--- son-gtkapi/spec/support/factory_girl.rb
+++ son-gtkapi/spec/support/factory_girl.rb
@@ -24,11 +24,11 @@
 ## acknowledge the contributions of their colleagues of the SONATA 
 ## partner consortium (www.sonata-nfv.eu).
 # spec/support/factory_girl.rb
-require 'factory_girl'
+require 'factory_bot'
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
-    FactoryGirl.find_definitions
+    FactoryBot.find_definitions
   end
 end
