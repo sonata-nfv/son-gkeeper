9287e7c1e0b7bce5b558b1eb5f6aa85b92ee378e
José Bonnet <jose.bonnet@gmail.com>
2017-12-21 16:19:50 +0000

    Added migration version

--- son-gtksrv/db/migrate/20170615140110_add_ingress_egress.rb
+++ son-gtksrv/db/migrate/20170615140110_add_ingress_egress.rb
@@ -1,4 +1,4 @@
-class AddIngressEgress < ActiveRecord::Migration
+class AddIngressEgress < ActiveRecord::Migration[4.2]
   def change
     add_column :requests, :ingress, :string
     add_column :requests, :egress, :string
