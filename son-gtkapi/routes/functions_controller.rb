3eea5a37c44f8a0b7d259d735e63cd4ab7cca2a6
José Bonnet <jose.bonnet@gmail.com>
2017-12-29 19:25:29 +0000

    Fixed wrong variable naming.. again

--- son-gtkapi/routes/functions_controller.rb
+++ son-gtkapi/routes/functions_controller.rb
@@ -56,8 +56,8 @@ class GtkApi < Sinatra::Base
       # request.env['QUERY_STRING']
       params.delete('captures') if params.keys.include?('captures')
       #functions = SONATA::CurbAdapter.find(url: ENV[GtkApi.services['functions']['env_var_url']], params: params, logger: GtkApi.logger)
-      function = FunctionManagerService.find(params)
-      validate_collection_existence(collection: function, name: 'functions', kpi_method: method(:count_functions_metadata_queries), began_at: began_at, log_message: log_message)
+      functions = FunctionManagerService.find(params)
+      validate_collection_existence(collection: functions, name: 'functions', kpi_method: method(:count_functions_metadata_queries), began_at: began_at, log_message: log_message)
       logger.debug(log_message) {"Found functions #{functions}"}
       keys_to_delete = {main_key: :vnfd, sub_keys: [:connection_points, :monitoring_rules, :virtual_deployment_units, :virtual_links]}
       filtered_functions = enhance_collection( collection: functions[:items], user: user_name, keys_to_delete: keys_to_delete)
