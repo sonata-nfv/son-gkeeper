7635f27f2ae149805526c4d735dbd4f14bc457f5
Felipe Vicens <felipe.vicens@atos.net>
2018-01-02 15:17:12 +0100

    Adding son-gtkapi build and test to Jenkinsfile

--- tests/unit/gtkpkg.sh
+++ tests/unit/gtkpkg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker run -i \
-
+--rm=true \
+--net=son-sp \
+--network-alias=son-gtkpkg \
 -e RACK_ENV=integration \
 -e CATALOGUES_URL=http://son-catalogue-repository:4011/catalogues/api/v2 \
 -v "$(pwd)/spec/reports/son-gtkpkg:/app/spec/reports" \
